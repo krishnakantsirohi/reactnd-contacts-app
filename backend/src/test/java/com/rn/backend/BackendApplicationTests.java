3af2c35600519750cbb1db1c76b45d13bd2a43f4
kk <sirohikrishnakant@gmail.com>
2020-06-03 11:45:36 -0500

    backend

---
+++ backend/src/test/java/com/rn/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rn.backend;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
